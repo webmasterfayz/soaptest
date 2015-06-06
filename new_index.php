dddd06bbbfe05a21a5bfca2656a9954e5d42e465
Md Monir Ahmed Fayz <webmasterfayz@gmail.com>
2015-06-06 15:38:26 +0600

    dd

--- new_index.php
+++ new_index.php
@@ -1,6 +1,7 @@
 <?php 
 echo "this is fayz";
-
+var t;
+t=10;
 ?>
 
 new_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.php
