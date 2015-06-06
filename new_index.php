114ffbb5b1fc61ad8571a725f8aaee1c5470ab3b
Enamul Haque <enamcse07@gmail.com>
2015-06-06 15:44:13 +0600

    Update new_index.php

--- new_index.php
+++ new_index.php
@@ -1,8 +1,8 @@
-check for conflict
+<!--check for conflict-->
 <?php 
 echo "this is fayz";
 var t;
 t=10;
 ?>
-new_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.php
-from remote
+<!--new_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.phpnew_index.php
+from remote-->
